e0d92da88c904330140f4704043849fbb9db3b5e
evg671ZXC <evgmine671@gmail.com>
2024-07-03 21:32:02 +0600

    secret added

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+load_dotenv()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 @app.route('/')
 def index():
